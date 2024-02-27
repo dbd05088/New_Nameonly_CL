ea24ce4f78da399130ac6448e7b9d2bda5ba2a69
dbd05088@naver.com <dbd05088@naver.com>
2024-02-27 16:08:08 +0900

    cifar10_web_10 added

--- dataset/cifar10_setting.sh
+++ dataset/cifar10_setting.sh
@@ -3,10 +3,13 @@ gdown --id "16DTKBJjjzU4Z3jmiIYwgEPrXb4Hju3B4"
 gdown --id "1VxdSntW9y6tzzSD1aiG_YGaL3jzpJv4g"
 gdown --id "1JsokoJg1ws0Pbz2HMiosBSpPh4gq7KWL"
 gdown --id "1gTJP9ciBwg-bpqdtvlbSDWEjNBQ4x4bu"
+gdown --id "179g6v9hOAwDKXrgQ7Gs3-5hifjVt0UAu"
+
 
 tar -xvf cifar10_MA.tar
 tar -xvf cifar10_generated.tar
 tar -xvf cifar10_web.tar
+tar -xvf cifar10_web_10.tar
 tar -xvf cifar10_nc.tar
 tar -xvf cifar10_c.tar
 
@@ -14,11 +17,13 @@ mkdir cifar10
 mv cifar10_MA cifar10
 mv cifar10_generated cifar10
 mv cifar10_web cifar10
+mv cifar10_web_10 cifar10
 mv cifar10_nc cifar10
 mv cifar10_c cifar10
 
 rm cifar10_MA.tar
 rm cifar10_generated.tar
 rm cifar10_web.tar
+rm cifar10_web_10.tar
 rm cifar10_nc.tar
 rm cifar10_c.tar
