b142345a91fe448cb614bba3958b20425bf58281
dbd05088 <dbd05088@naver.com>
2024-02-29 00:40:18 +0900

    DomainNet gdrive updated

---
+++ dataset/DomainNet_gdrive.sh
@@ -0,0 +1,16 @@
+gdrive files download 1I5W7bL46cduMMrNq4gH5KsUOonLejMuZ
+gdrive files download 1G_aILsxwKAhfFPwSuc2ecnuhE9JH1RH-
+gdrive files download 1UDKD-volVNLmujzmdBGIBDhbc2o1-wsf
+
+tar -xvf DomainNet_MA.tar
+tar -xvf DomainNet_generated.tar
+tar -xvf DomainNet_web.tar
+
+mkdir DomainNet
+mv DomainNet_MA DomainNet
+mv DomainNet_generated DomainNet
+mv DomainNet_web DomainNet
+
+rm DomainNet_generated.tar
+rm DomainNet_MA.tar
+rm DomainNet_web.tar
