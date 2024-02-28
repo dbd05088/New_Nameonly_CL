d56b4fc445b8cf2b25b44cb83df529cbbffa626c
dbd05088 <dbd05088@naver.com>
2024-02-29 00:42:32 +0900

    OfficeHome

---
+++ dataset/OfficeHome_gdrive.sh
@@ -0,0 +1,16 @@
+gdrive files download 1dqP7fEr8z5FvTw_rnlkxBM-eoJK0IcON
+gdrive files download 1mXLknDb--GavWYD3vlEmdlU7E-5se0ER
+gdrive files download 1U3oFKEeVI7gXORCVz1Tij56b4vebN55Q
+
+tar -xvf OfficeHome_MA.tar
+tar -xvf OfficeHome_generated.tar
+tar -xvf OfficeHome_web.tar
+
+mkdir OfficeHome
+mv OfficeHome_MA OfficeHome
+mv OfficeHome_generated OfficeHome
+mv OfficeHome_web OfficeHome
+
+rm OfficeHome_generated.tar
+rm OfficeHome_MA.tar
+rm OfficeHome_web.tar
