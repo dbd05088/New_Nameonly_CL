6dca6d54eab69c179c1e4bdc3a9f7def425d0d08
dbd05088@naver.com <dbd05088@naver.com>
2024-03-02 22:48:43 +0900

    cct, PACS shell file updated

---
+++ dataset/cct_gdrive.sh
@@ -0,0 +1,28 @@
+gdrive files download 1crd9Lkb7S0H5AlvlxoLxLxUKQRaRqhJk 
+gdrive files download 1eozSk_TV65QgZ7LB6qa2_ObjC639qMTM
+gdrive files download 18BDlPQR5gktey3rUccFeRczeF5FId9pa
+gdrive files download 1VVXJvoo-5FWKABYbd8nWCVE4exqZbf11
+gdrive files download 13_Siy5m-aj_mEVvugPsmSAoZRJIJz9H8
+gdrive files download 1mClS-e1TGE6iXMLgfhieisBSN4kUaKRy
+
+tar -xvf cct_train_ma.tar
+tar -xvf cct_in_test_ma.tar
+tar -xvf cct_out_test_ma.tar
+tar -xvf cct_web.tar
+tar -xvf cct_web_10.tar
+tar -xvf cct_sdxl_diversified.tar
+
+mkdir cct
+mv cct_train_ma cct
+mv cct_in_test_ma cct
+mv cct_out_test_ma cct
+mv cct_web_10 cct
+mv cct_web cct
+mv cct_sdxl_diversified cct
+
+rm cct_train_ma.tar
+rm cct_in_test_ma.tar
+rm cct_out_test_ma.tar
+rm cct_web.tar
+rm cct_web_10.tar
+rm cct_sdxl_diversified.tar
