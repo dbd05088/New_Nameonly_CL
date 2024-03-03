a29392c97516c4413f42220b119f2dd1982def3b
dbd05088@naver.com <dbd05088@naver.com>
2024-03-03 15:43:47 +0900

    DomainNet added

--- dataset/DomainNet_gdrive.sh
+++ dataset/DomainNet_gdrive.sh
@@ -1,14 +1,14 @@
 gdrive files download 1eCHHO8eheuM-FT0t13LbmOoTGxM0e-jO
 gdrive files download 1Gnqna2AjQk-wVIwogGWS04MCvYnTmkdi
-gdrive files download 1UDKD-volVNLmujzmdBGIBDhbc2o1-wsf
 gdrive files download 1VNIz2xPm6ICKzp8i7KhhyN2VYv4fkQZY
 gdrive files download 1I5W7bL46cduMMrNq4gH5KsUOonLejMuZ
 gdrive files download 1G_aILsxwKAhfFPwSuc2ecnuhE9JH1RH-
+gdrive files download 1sZdh04o80X8xKNBSXsnOe1VBlhk1f1Oy
 
 tar -xvf DomainNet_train_ma.tar
 tar -xvf DomainNet_test_ma.tar
 tar -xvf DomainNet_sdxl_diversified.tar
-tar -xvf DomainNet_web.tar
+tar -xvf DomainNet_web2.tar
 tar -xvf DomainNet_MA.tar
 tar -xvf DomainNet_generated.tar
 
@@ -16,7 +16,7 @@ mkdir DomainNet
 mv DomainNet_MA DomainNet
 mv DomainNet_train_ma DomainNet
 mv DomainNet_test_ma DomainNet
-mv DomainNet_web DomainNet
+mv DomainNet_web2 DomainNet
 mv DomainNet_sdxl_diversified DomainNet
 mv DomainNet_generated DomainNet
 
@@ -24,5 +24,5 @@ rm DomainNet_MA.tar
 rm DomainNet_sdxl_diversified.tar
 rm DomainNet_train_ma.tar
 rm DomainNet_test_ma.tar
-rm DomainNet_web.tar
+rm DomainNet_web2.tar
 rm DomainNet_generated.tar
