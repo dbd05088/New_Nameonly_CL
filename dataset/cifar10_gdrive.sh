f80f66be307484c177ebc02153a8ca2d39ebbc6a
dbd05088@naver.com <dbd05088@naver.com>
2024-03-07 06:59:28 +0900

    cifar10 added

--- dataset/cifar10_gdrive.sh
+++ dataset/cifar10_gdrive.sh
@@ -1,36 +1,52 @@
 #nohup gdrive files download 179g6v9hOAwDKXrgQ7Gs3-5hifjVt0UAu #web_10
-gdrive files download 1gTJP9ciBwg-bpqdtvlbSDWEjNBQ4x4bu #web
-gdrive files download 1W6YRfL-u9ySIdXWkHiQ0Whq9m8EtzQhb #generated
-gdrive files download 1VxdSntW9y6tzzSD1aiG_YGaL3jzpJv4g #nc
-gdrive files download 16DTKBJjjzU4Z3jmiIYwgEPrXb4Hju3B4 #c
-gdrive files download 1JsokoJg1ws0Pbz2HMiosBSpPh4gq7KWL #ma
-gdrive files download 1TdIXr9uwdCcjDPBmnD_VQ2qv30uNKyxQ #original test set
-gdrive files download 1WIbwg_wV7DIQvGbsctqZy6uBA4yeDcIN #sdxl_diversified
+# gdrive files download 1gTJP9ciBwg-bpqdtvlbSDWEjNBQ4x4bu #web
+# gdrive files download 1W6YRfL-u9ySIdXWkHiQ0Whq9m8EtzQhb #generated
+# gdrive files download 1VxdSntW9y6tzzSD1aiG_YGaL3jzpJv4g #nc
+# gdrive files download 16DTKBJjjzU4Z3jmiIYwgEPrXb4Hju3B4 #c
+# gdrive files download 1JsokoJg1ws0Pbz2HMiosBSpPh4gq7KWL #ma
+# gdrive files download 1TdIXr9uwdCcjDPBmnD_VQ2qv30uNKyxQ #original test set
+# gdrive files download 1WIbwg_wV7DIQvGbsctqZy6uBA4yeDcIN #sdxl_diversified
 
-tar -xvf cifar10_MA.tar
-tar -xvf cifar10_generated.tar
-tar -xvf cifar10_web.tar
-tar -xvf cifar10_web_10.tar
-tar -xvf cifar10_original_test.tar
-tar -xvf cifar10_nc.tar
-tar -xvf cifar10_c.tar
-tar -xvf cifar10_sdxl_diversified.tar
+# tar -xvf cifar10_MA.tar
+# tar -xvf cifar10_generated.tar
+# tar -xvf cifar10_web.tar
+# tar -xvf cifar10_web_10.tar
+# tar -xvf cifar10_original_test.tar
+# tar -xvf cifar10_nc.tar
+# tar -xvf cifar10_c.tar
+# tar -xvf cifar10_sdxl_diversified.tar
 
-mkdir cifar10
-mv cifar10_MA cifar10
-mv cifar10_generated cifar10
-mv cifar10_web cifar10
-mv cifar10_web_10 cifar10
-mv cifar10_original_test cifar10
-mv cifar10_nc cifar10
-mv cifar10_c cifar10
-mv cifar10_sdxl_diversified cifar10
+# mkdir cifar10
+# mv cifar10_MA cifar10
+# mv cifar10_generated cifar10
+# mv cifar10_web cifar10
+# mv cifar10_web_10 cifar10
+# mv cifar10_original_test cifar10
+# mv cifar10_nc cifar10
+# mv cifar10_c cifar10
+# mv cifar10_sdxl_diversified cifar10
 
-rm cifar10_MA.tar
-rm cifar10_generated.tar
-rm cifar10_original_test.tar
-rm cifar10_web.tar
-rm cifar10_web_10.tar
-rm cifar10_nc.tar
-rm cifar10_c.tar
-rm cifar10_sdxl_diversified.tar
+# rm cifar10_MA.tar
+# rm cifar10_generated.tar
+# rm cifar10_original_test.tar
+# rm cifar10_web.tar
+# rm cifar10_web_10.tar
+# rm cifar10_nc.tar
+# rm cifar10_c.tar
+# rm cifar10_sdxl_diversified.tar
+
+gdrive files download 1wNCrBaer-R14cFuWH8j7Ox0LWWU-elXP
+gdrive files download 1Hs47yW5EQHEmsd2jXUl6k7hfTiCtiyED
+gdrive files download 1CYhe5krOeJhRSaHrzg2VxT_NHKICBUPf
+
+tar -xvf cifar10_RMD_temp_0_5.tar
+tar -xvf cifar10_RMD_temp_1.tar
+tar -xvf cifar10_RMD_temp_3.tar
+
+mv cifar10_RMD_temp_0_5 cifar10
+mv cifar10_RMD_temp_1 cifar10
+mv cifar10_RMD_temp_3 cifar10
+
+rm cifar10_RMD_temp_0_5.tar
+rm cifar10_RMD_temp_1.tar
+rm cifar10_RMD_temp_3.tar
