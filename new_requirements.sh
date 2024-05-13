25c68f98f5bffc5d7530cc887a8ace7f28cff4d4
seongwoncho <87466978+seongwon980@users.noreply.github.com>
2024-05-13 13:21:38 +0900

    Create new_requirements.sh

---
+++ new_requirements.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+pip install tensorboard
+pip install pandas
+pip install kornia
+pip install opencv-contrib-python
+pip install torch-optimizer
+pip install easydict
+pip install onedrivedownloader
+pip install timm
+pip install faiss-cpu==1.8.0
+
+apt install libgl1-mesa-glx
