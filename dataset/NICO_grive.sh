dc3dac17b10bd791cc7c724c124e62785ee7c82f
98minjae <reccos1020@gmail.com>
2024-05-19 00:46:56 +0900

    NICO_glide updated

--- dataset/NICO_grive.sh
+++ dataset/NICO_grive.sh
@@ -1,10 +1,16 @@
 mkdir -p NICO
-gdrive files download 1eU5Nh4UrMXGYTCFS1y56K2ZFqV4-BX8g
-tar -xf NICO_MA.tar
-mv NICO_MA NICO/
-rm NICO_MA.tar
+# gdrive files download 1eU5Nh4UrMXGYTCFS1y56K2ZFqV4-BX8g
+# tar -xf NICO_MA.tar
+# mv NICO_MA NICO/
+# rm NICO_MA.tar
 
-gdrive files download 13liukLcnRtQ5ueQd_EcIRbbxtvr17F56
-tar -xf NICO_sdxl.tar
-mv NICO_sdxl NICO/
-rm NICO_sdxl.tar
+# gdrive files download 13liukLcnRtQ5ueQd_EcIRbbxtvr17F56
+# tar -xf NICO_sdxl.tar
+# mv NICO_sdxl NICO/
+# rm NICO_sdxl.tar
+
+gdrive files download 1bJ9lnYYZqdxlOd9IFgEWXzxD6ON3EOht
+tar -xf NICO_glide.tar
+mv NICO_glide NICO/
+rm NICO_glide.tar
+
