91e532c7e9ec2b296589819a67c8673ab5588469
soengwon980 <poiuy98749@yonsei.ac.kr>
2024-05-21 22:44:35 +0900

    NICO generated (sdxl_diversified, equalweight, RMD)

--- dataset/NICO_grive.sh
+++ dataset/NICO_grive.sh
@@ -9,12 +9,25 @@ mkdir -p NICO
 # mv NICO_sdxl NICO/
 # rm NICO_sdxl.tar
 
-gdrive files download 1OCbaLGlTF1KNyMWgiXM3TsmLG54d-e1s
-tar -xf NICO_glide.tar
-mv NICO_glide NICO/
-rm NICO_glide.tar
+# gdrive files download 1OCbaLGlTF1KNyMWgiXM3TsmLG54d-e1s
+# tar -xf NICO_glide.tar
+# mv NICO_glide NICO/
+# rm NICO_glide.tar
 
-gdrive files download 1O-okwN3x6mFJvMuGSwQQOynSdOkRNyV7
-tar -xf NICO_sdxl_base_filtered.tar
-mv NICO_sdxl_base_filtered NICO/
-rm NICO_sdxl_base_filtered.tar
+# gdrive files download 1O-okwN3x6mFJvMuGSwQQOynSdOkRNyV7
+# tar -xf NICO_sdxl_base_filtered.tar
+# mv NICO_sdxl_base_filtered NICO/
+# rm NICO_sdxl_base_filtered.tar
+
+gdrive files download 1NiUOkMDzL6tV2ZckyGHiaL37w3-pyvv5
+gdrive files download 1g2l6AdU68UF4xIQjv68SD1_uWAoV7zt_
+gdrive files download 1eTBp_0dFCcEn_M-yXctqx_VFzUbQ8N2I
+tar -xf NICO_sdxl.tar
+tar -xf NICO_generated_equalweight.tar
+tar -xf NICO_generated_RMD_w_normalize_clip_90_temp_0_5.tar
+mv NICO_sdxl NICO/
+mv NICO_generated_equalweight NICO/
+mv NICO_generated_RMD_w_normalize_clip_90_temp_0_5 NICO/
+rm NICO_sdxl.tar
+rm NICO_generated_equalweight.tar
+rm NICO_generated_RMD_w_normalize_clip_90_temp_0_5.tar
