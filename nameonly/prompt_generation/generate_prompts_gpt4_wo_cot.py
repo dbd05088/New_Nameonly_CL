4c1eec13ee996d02bf1252d6ccbd7d9e0a592255
soengwon980 <poiuy98749@yonsei.ac.kr>
2024-05-22 02:04:57 +0900

    DomainNet Web RMD

---
+++ nameonly/prompt_generation/generate_prompts_gpt4_wo_cot.py
@@ -0,0 +1,103 @@
+import re
+import pickle
+import json
+from openai import OpenAI
+from tqdm import tqdm
+from classes import *
+
+# Prompt format에 맞도록 수정해야 함 (0516)
+
+def generate_prompt_stage1(client):
+    base_message = f"To generate images using a text-to-image generation model, I need to create a prompt. Keep the domain photorealistic and use different visual scenes and visual styles or different color profiles/ palettes."
+    end_message = f"\nPlease create one prompt sentence (under 10 words) that fits this description. Please ensure the response format is strictly 'prompt: answer' and include the word '[concept].\n"
+    
+    base_message += end_message
+
+    # Generate one prompt using GPT-4 API
+    response = client.chat.completions.create(
+    model="gpt-4-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": base_message},
+            {"role": "assistant", "content": "prompt: A photo of [concept] in neutral tones."},
+            {"role": "user", "content": base_message},
+        ]
+    )
+
+    response_content = response.choices[0].message.content
+    match = re.search(r'prompt:\s*(.*)', response_content, re.IGNORECASE)
+    if match:
+        return match.group(1)
+    else:
+        return response_content
+
+def generate_prompt_stage2(client, metaprompt):
+    base_message = f"To generate images using a text-to-image generation model, I need to create a prompt. Keep the domain photorealistic and use different visual scenes and visual styles or different color profiles/ palettes. The prompt should be similar to '{metaprompt}' but slightly different."
+    
+    end_message = f"\nPlease create one prompt sentence (under 15 words) that fits this description. Please ensure the response format is strictly 'prompt: answer' and include the word '[concept].\n"
+    
+    base_message += end_message
+
+    # Generate one prompt using GPT-4 API
+    response = client.chat.completions.create(
+    model="gpt-4-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": base_message},
+            {"role": "assistant", "content": "prompt: A photo of [concept] in neutral tones"},
+            {"role": "user", "content": base_message}
+        ]
+    )
+
+    response_content = response.choices[0].message.content
+    match = re.search(r'prompt:\s*(.*)', response_content, re.IGNORECASE)
+    if match:
+        return match.group(1)
+    else:
+        return response_content
+
+cls_count_dict = PACS_count
+metaprompt_json_path = './prompts/temp_base_metaprompts.json'
+totalprompt_json_path = './prompts/static_totalprompts_with_cot_50_wo_cot.json'
+
+client = OpenAI(api_key="sk-proj-b6mF6aJroOzev4yh1afBT3BlbkFJcgqlS8S3hrxASu62u3a6")
+classes = list(cls_count_dict.keys())
+num_metaprompts = 10
+num_prompts_per_metaprompt = 5
+
+# metaprompts = ['A photo of a [concept].']
+# for i in tqdm(range(num_metaprompts - 1)):
+#     try:
+#         prompt = generate_prompt_stage1(client)
+#         print(f"Previous prompt list: {metaprompts}")
+#         print(f"Generated metaprompt for stage: {prompt}")
+#         metaprompts.append(prompt)
+#     except:
+#         pass
+
+# with open(metaprompt_json_path, 'w') as f:
+#     json.dump(metaprompts, f)
+
+
+# Diversified prompt generation
+# Load metaprompt pickle file
+with open(metaprompt_json_path, 'r') as f:
+    metaprompt_list = json.load(f)
+
+totalprompt_dict = {'metaprompts': []}
+for i, metaprompt in enumerate(tqdm(metaprompt_list)):
+    metaprompt_dict = {'index': i, 'metaprompt': metaprompt, 'prompts': []}
+    diversified_prompts = []
+    for j in range(num_prompts_per_metaprompt):
+        try:
+            prompt = generate_prompt_stage2(client, metaprompt)
+            print(f"previous generated prompts: {diversified_prompts}")
+            print(f"Generated prompt: {prompt}")
+            diversified_prompts.append(prompt)
+            metaprompt_dict['prompts'].append({'index': j, 'content': prompt})
+        except:
+            print(f"Error while generating diversified prompts for {metaprompt}")
+    totalprompt_dict['metaprompts'].append(metaprompt_dict)
+
+with open(totalprompt_json_path, 'w') as f:
+    json.dump(totalprompt_dict, f)
