43f806614533cccc54da7f12fbc6f1c488faf786
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-05-28 16:05:36 +0900

    Refactor: ex.sh

--- ex.sh
+++ ex.sh
@@ -10,11 +10,20 @@
 
 # CIL CONFIG
 # NOTE="imagenet_sdp_sigma0_mem_10000_iter_0.125"
-NOTE="rebuttal4_vit_er_cct_iter2_mem200"
-MODE="er"
 
-K_COEFF="4"
-TEMPERATURE="0.125"
+# --------------------------IMPORTANT-------------------------- #
+MODE="er"
+MODEL_NAME="resnet18"
+DATASET="cct" # cifar10, cifar100, tinyimagenet, imagenet
+NOTE="iclr_${MODEL_NAME}_${DATASET}_${MODE}"
+TYPES=("ma" "static_cot_50_sdxl", "generated_equalweight")
+SEEDS="5"
+GPUS=("4" "1" "2" "3" "4")
+# --------------------------IMPORTANT-------------------------- #
+echo "MODE: $MODE"
+echo "MODEL_NAME: $MODEL_NAME"
+echo "DATASET: $DATASET"
+echo "NOTE: $NOTE"
 
 TRANSFORM_ON_GPU="--transform_on_gpu"
 N_WORKER=4
@@ -24,11 +33,10 @@ EVAL_BATCH_SIZE=500
 #USE_KORNIA="--use_kornia"
 USE_KORNIA=""
 UNFREEZE_RATE=0.5
-SEEDS="5"
 DATA_DIR=""
+K_COEFF="4"
+TEMPERATURE="0.125"
 
-GPUS=("4" "1" "2" "3" "4")
-DATASET="cct" # cifar10, cifar100, tinyimagenet, imagenet
 SIGMA=0
 REPEAT=1
 INIT_CLS=100
@@ -36,90 +44,21 @@ USE_AMP=""
 
 if [ "$DATASET" == "cifar10" ]; then
     MEM_SIZE=1000
-    TYPES=("ma" "static_cot_50_sdxl", "generated_equalweight") #("web_10" "ma" "generated" "web" "web_10")
     N_SMP_CLS="9" K="3" MIR_CANDS=50
     CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="resnet18" VAL_PERIOD=500 EVAL_PERIOD=500
+    VAL_PERIOD=500 EVAL_PERIOD=500
     BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
     BASEINIT_SAMPLES=6000 FEAT_DIM=14 FEAT_MEM_SIZE=24000
     SAMPLES_PER_TASK=20000 
     ONLINE_ITER=2
     EVAL_POINT="1000 2000 3000 4000 5000"
 
-elif [ "$DATASET" == "aircraft" ]; then
-    MEM_SIZE=800
-    TYPES=("sdxl_diversified" "web" "train_ma")
-    N_SMP_CLS="9" K="3" MIR_CANDS=50
-    CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="vit" VAL_PERIOD=500 EVAL_PERIOD=100
-    BATCHSIZE=32; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
-    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
-    SAMPLES_PER_TASK=2000
-    ONLINE_ITER=3
-    if [ "$SEEDS" == "1" ]; then
-        EVAL_POINT="1335 2669 3997 5332 6667"
-    elif [ "$SEEDS" == "2" ]; then
-        EVAL_POINT="1331 2665 3998 5334 6667"
-    elif [ "$SEEDS" == "3" ]; then
-        EVAL_POINT="1335 2665 4001 5334 6667"
-    elif [ "$SEEDS" == "4" ]; then
-        EVAL_POINT="1331 2666 4001 5331 6667"
-    elif [ "$SEEDS" == "5" ]; then
-        EVAL_POINT="1332 2665 3999 5330 6667"
-    fi
-
-elif [ "$DATASET" == "food101" ]; then
-    MEM_SIZE=2000
-    TYPES=("train_ma" "sdxl_diversified_newprompt" "web")
-    N_SMP_CLS="9" K="3" MIR_CANDS=50
-    CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="resnet18" VAL_PERIOD=500 EVAL_PERIOD=500
-    BATCHSIZE=32; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
-    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
-    SAMPLES_PER_TASK=2000
-    ONLINE_ITER=3
-    EVAL_POINT="3000 6000 9000 12000 15150"
-
-elif [ "$DATASET" == "cct" ]; then
-    MEM_SIZE=400
-    TYPES=("web_RMD_w_normalize_clip_90_temp_0_5") #"train_ma" "sdxl_diversified" "web" "generated" "sdxl_diversified_nofiltering" "RMD_classwise_temp_0_5" "RMD_classwise_temp_1" "RMD_classwise_temp_3"
-    N_SMP_CLS="9" K="3" MIR_CANDS=50
-    CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="vit" VAL_PERIOD=500 EVAL_PERIOD=100
-    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
-    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
-    SAMPLES_PER_TASK=2000
-    ONLINE_ITER=2
-    EVAL_POINT="600 1200 1800 2400"
-
-elif [ "$DATASET" == "PACS" ]; then
-    MEM_SIZE=200
-    TYPES=("sdxl_diversified")
-    N_SMP_CLS="9" K="3" MIR_CANDS=50
-    CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="vit" VAL_PERIOD=500 EVAL_PERIOD=100
-    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
-    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
-    SAMPLES_PER_TASK=2000
-    ONLINE_ITER=2
-    if [ "$SEEDS" == "1" ]; then
-        EVAL_POINT="668 1282 1670"
-    elif [ "$SEEDS" == "2" ]; then
-        EVAL_POINT="648 1036 1670"
-    elif [ "$SEEDS" == "3" ]; then
-        EVAL_POINT="573 1039 1670"
-    elif [ "$SEEDS" == "4" ]; then
-        EVAL_POINT="557 1269 1670"
-    elif [ "$SEEDS" == "5" ]; then
-        EVAL_POINT="570 1282 1670"
-    fi
-
 elif [ "$DATASET" == "PACS_final" ]; then
     MEM_SIZE=200
-    TYPES=("sdbp") # web_RMD_temp_0_5_W web_inverse_temp_0_5_WF "web_RMD_temp_3" "inverseprob" "bottomk" "topk") #"sampling_4" "sampling_2" "sampling_0_5" "sampling_0_25" "sampling_0_125" #"equalweighted" "ensembled_RMD_0_5_modelwise" "ensembled_RMD_1_modelwise" "ensembled_RMD_3_modelwise" "ensembled_RMD_classwise_temp_0_5" "ensembled_RMD_classwise_temp_1" "PACS_final_ensembled_RMD_classwise_temp_3") #("ensembled_samplewise_RMD_0_5" "ensembled_samplewise_RMD_1" "ensembled_samplewise_RMD_3" "ensembled_samplewise_RMD_5") #("ensembled_RMD_temp1" "ensembled_RMD_temp2" "ensembled_RMD_temp5" "ensembled_RMD_temp10")
+    # TYPES=("sdbp") # web_RMD_temp_0_5_W web_inverse_temp_0_5_WF "web_RMD_temp_3" "inverseprob" "bottomk" "topk") #"sampling_4" "sampling_2" "sampling_0_5" "sampling_0_25" "sampling_0_125" #"equalweighted" "ensembled_RMD_0_5_modelwise" "ensembled_RMD_1_modelwise" "ensembled_RMD_3_modelwise" "ensembled_RMD_classwise_temp_0_5" "ensembled_RMD_classwise_temp_1" "PACS_final_ensembled_RMD_classwise_temp_3") #("ensembled_samplewise_RMD_0_5" "ensembled_samplewise_RMD_1" "ensembled_samplewise_RMD_3" "ensembled_samplewise_RMD_5") #("ensembled_RMD_temp1" "ensembled_RMD_temp2" "ensembled_RMD_temp5" "ensembled_RMD_temp10")
     N_SMP_CLS="9" K="3" MIR_CANDS=50
     CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="resnet18" VAL_PERIOD=500 EVAL_PERIOD=100
+    VAL_PERIOD=500 EVAL_PERIOD=100
     BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
     BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
     SAMPLES_PER_TASK=2000
@@ -137,74 +76,24 @@ elif [ "$DATASET" == "PACS_final" ]; then
         EVAL_POINT="455 764 1333"
     fi
 
-    # 0_5
-    # if [ "$SEEDS" == "1" ]; then
-    #     EVAL_POINT="228 380 664"
-    # elif [ "$SEEDS" == "2" ]; then
-    #     EVAL_POINT="318 472 664"
-    # elif [ "$SEEDS" == "3" ]; then
-    #     EVAL_POINT="259 505 664"
-    # elif [ "$SEEDS" == "4" ]; then
-    #     EVAL_POINT="319 473 664"
-    # elif [ "$SEEDS" == "5" ]; then
-    #     EVAL_POINT="226 380 664"
-    # fi
-
-    # 0_25
-    # if [ "$SEEDS" == "1" ]; then
-    #     EVAL_POINT="113 189 331"
-    # elif [ "$SEEDS" == "2" ]; then
-    #     EVAL_POINT="159 235 331"
-    # elif [ "$SEEDS" == "3" ]; then
-    #     EVAL_POINT="129 252 331"
-    # elif [ "$SEEDS" == "4" ]; then
-    #     EVAL_POINT="159 236 331"
-    # elif [ "$SEEDS" == "5" ]; then
-    #     EVAL_POINT="112 189 331"
-    # fi
-
-    # 0_125
-    # if [ "$SEEDS" == "1" ]; then
-    #     EVAL_POINT="55 93 164"
-    # elif [ "$SEEDS" == "2" ]; then
-    #     EVAL_POINT="79 116 164"
-    # elif [ "$SEEDS" == "3" ]; then
-    #     EVAL_POINT="64 125 164"
-    # elif [ "$SEEDS" == "4" ]; then
-    #     EVAL_POINT="79 117 164"
-    # elif [ "$SEEDS" == "5" ]; then
-    #     EVAL_POINT="55 93 164"
-    # fi
-
-
-elif [ "$DATASET" == "OfficeHome" ]; then
-    MEM_SIZE=500
-    TYPES=("train_ma" "generated" "web")
+elif [ "$DATASET" == "cct" ]; then
+    MEM_SIZE=400
+    # TYPES=("web_RMD_w_normalize_clip_90_temp_0_5") #"train_ma" "sdxl_diversified" "web" "generated" "sdxl_diversified_nofiltering" "RMD_classwise_temp_0_5" "RMD_classwise_temp_1" "RMD_classwise_temp_3"
     N_SMP_CLS="9" K="3" MIR_CANDS=50
     CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="resnet18" VAL_PERIOD=500 EVAL_PERIOD=100
-    BATCHSIZE=32; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
-    BASEINIT_SAMPLES=2614 FEAT_DIM=14 FEAT_MEM_SIZE=9600
+    VAL_PERIOD=500 EVAL_PERIOD=100
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
     SAMPLES_PER_TASK=2000
     ONLINE_ITER=2
-    if [ "$SEEDS" == "1" ]; then
-        EVAL_POINT="715 1367 2003 2693 3459"
-    elif [ "$SEEDS" == "2" ]; then
-        EVAL_POINT="714 1395 2068 2744 3459"
-    elif [ "$SEEDS" == "3" ]; then
-        EVAL_POINT="634 1356 2075 2772 3459"
-    elif [ "$SEEDS" == "4" ]; then
-        EVAL_POINT="727 1395 2105 2842 3459"
-    elif [ "$SEEDS" == "5" ]; then
-        EVAL_POINT="621 1317 2002 2740 3459"
-    fi
+    EVAL_POINT="600 1200 1800 2400"
 
 elif [ "$DATASET" == "DomainNet" ]; then
     MEM_SIZE=8000
-    TYPES=("sdbp") # "newsample_equalweight"
+    # TYPES=("sdbp") # "newsample_equalweight"
     N_SMP_CLS="9" K="3" MIR_CANDS=50
     CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="vit" VAL_PERIOD=500 EVAL_PERIOD=4000
+    VAL_PERIOD=500 EVAL_PERIOD=4000
     BATCHSIZE=64; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
     BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
     SAMPLES_PER_TASK=2000
@@ -223,10 +112,10 @@ elif [ "$DATASET" == "DomainNet" ]; then
 
 elif [ "$DATASET" == "NICO" ]; then
     MEM_SIZE=800 #1500
-    TYPES=("sdbp") # "newsample_equalweight"
+    # TYPES=("sdbp") # "newsample_equalweight"
     N_SMP_CLS="9" K="3" MIR_CANDS=50
     CANDIDATE_SIZE=50 VAL_SIZE=5
-    MODEL_NAME="resnet18" VAL_PERIOD=500 EVAL_PERIOD=500
+    VAL_PERIOD=500 EVAL_PERIOD=500
     BATCHSIZE=32; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
     BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
     SAMPLES_PER_TASK=960
