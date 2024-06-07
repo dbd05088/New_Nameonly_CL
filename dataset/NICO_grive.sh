82d2ef49b19f6d009c299cdaadf182ffacb00267
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-06-07 14:44:53 +0900

    Fix: gdrive

--- dataset/NICO_grive.sh
+++ dataset/NICO_grive.sh
@@ -1,38 +1,41 @@
+./gdrive account remove dbd05088@naver.com
+./gdrive account import gdrive_export-dbd05088_naver_com.tar
+./gdrive account switch dbd05088@naver.com
+
 mkdir -p NICO
-# gdrive files download 1eU5Nh4UrMXGYTCFS1y56K2ZFqV4-BX8g
-# tar -xf NICO_MA.tar
-# mv NICO_MA NICO/
+# ./gdrive files download 1eU5Nh4UrMXGYTCFS1y56K2ZFqV4-BX8g
+# tar -xf NICO_MA.tar -C NICO
 # rm NICO_MA.tar
 
-# gdrive files download 13liukLcnRtQ5ueQd_EcIRbbxtvr17F56
-# tar -xf NICO_sdxl.tar
-# mv NICO_sdxl NICO/
+
+# ./gdrive files download 13liukLcnRtQ5ueQd_EcIRbbxtvr17F56
+# tar -xf NICO_sdxl.tar -C NICO
 # rm NICO_sdxl.tar
 
-# gdrive files download 1OCbaLGlTF1KNyMWgiXM3TsmLG54d-e1s
-# tar -xf NICO_glide.tar
-# mv NICO_glide NICO/
+
+# ./gdrive files download 1OCbaLGlTF1KNyMWgiXM3TsmLG54d-e1s
+# tar -xf NICO_glide.tar -C NICO
 # rm NICO_glide.tar
 
-# gdrive files download 1O-okwN3x6mFJvMuGSwQQOynSdOkRNyV7
-# tar -xf NICO_sdxl_base_filtered.tar
-# mv NICO_sdxl_base_filtered NICO/
+
+# ./gdrive files download 1O-okwN3x6mFJvMuGSwQQOynSdOkRNyV7
+# tar -xf NICO_sdxl_base_filtered.tar -C NICO
 # rm NICO_sdxl_base_filtered.tar
 
-# gdrive files download 1NiUOkMDzL6tV2ZckyGHiaL37w3-pyvv5
-# gdrive files download 1g2l6AdU68UF4xIQjv68SD1_uWAoV7zt_
-# gdrive files download 1eTBp_0dFCcEn_M-yXctqx_VFzUbQ8N2I
-# tar -xf NICO_sdxl.tar
-# tar -xf NICO_generated_equalweight.tar
-# tar -xf NICO_generated_RMD_w_normalize_clip_90_temp_0_5.tar
-# mv NICO_sdxl NICO/
-# mv NICO_generated_equalweight NICO/
-# mv NICO_generated_RMD_w_normalize_clip_90_temp_0_5 NICO/
+
+
+# ./gdrive files download 1NiUOkMDzL6tV2ZckyGHiaL37w3-pyvv5
+# ./gdrive files download 1g2l6AdU68UF4xIQjv68SD1_uWAoV7zt_
+# ./gdrive files download 1eTBp_0dFCcEn_M-yXctqx_VFzUbQ8N2I
+# tar -xf NICO_sdxl.tar -C NICO
+# tar -xf NICO_generated_equalweight.tar -C NICO
+# tar -xf NICO_generated_RMD_w_normalize_clip_90_temp_0_5.tar -C NICO
 # rm NICO_sdxl.tar
 # rm NICO_generated_equalweight.tar
 # rm NICO_generated_RMD_w_normalize_clip_90_temp_0_5.tar
 
-gdrive files download 1Y4YEGyWt5dP6g77wif1t7_HKqMN8tlnj
-tar -xf NICO_sdbp.tar
-mv NICO_sdbp NICO/
+
+
+./gdrive files download 1Y4YEGyWt5dP6g77wif1t7_HKqMN8tlnj
+tar -xf NICO_sdbp.tar -C NICO
 rm NICO_sdbp.tar
