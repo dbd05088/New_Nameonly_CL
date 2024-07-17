37df36d001247e19f5c67c9007f0754aa664933d
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-07-17 16:25:55 +0900

    Add: floyd config

--- nameonly/generate_twostage/cogview_run.sh
+++ nameonly/generate_twostage/cogview_run.sh
@@ -4,7 +4,7 @@
 
 # Floyd: 1분에 2개정도 -> DomainNet 180개 -> 1시간 반, 5개 class 7시간 반
 # Cogview2: 72분에 class 1개, 1분에 3.57개, initialize는 7분정도 걸림.
-# 1개에 0.2784분 -> 1개 class에 30분 정도 걸린다고 보면 된다. 20개 class정도 생성하면 될듯.
+# 1개에 0.2784분 -> 1개 class에 30분 정도 걸린다고 보면 된다. 40개 class정도 생성하면 될듯.
 
 #SBATCH -p suma_a6000
 #SBATCH --gres=gpu:1
@@ -17,4 +17,4 @@ conda activate cogview
 cd Image-Local-Attention
 CUDA_HOME=/opt/ohpc/pub/apps/cuda/12.5 python setup.py install
 cd ../
-CUDA_HOME=/opt/ohpc/pub/apps/cuda/12.5 python get_image_onestage.py --config_path ./configs/PACS_cogview2.yaml --start_class 6 --end_class 6
+CUDA_HOME=/opt/ohpc/pub/apps/cuda/12.5 python get_image_onestage.py --config_path ./configs/DomainNet_cogview2.yaml --start_class 320 --end_class 344
