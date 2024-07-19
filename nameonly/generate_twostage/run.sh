0caf28d0c9726c58b88538c85eb2d022df582c99
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-07-19 10:55:05 +0900

    Add: kandinsky2

--- nameonly/generate_twostage/run.sh
+++ nameonly/generate_twostage/run.sh
@@ -11,4 +11,4 @@ ml purge
 
 conda init bash
 conda activate generate
-python get_image_onestage.py --config_path ./configs/floyd.yaml --start_class 6 --end_class 6
+python get_image_onestage.py --config_path ./configs/kandinsky2.yaml --start_class 0 --end_class 0
