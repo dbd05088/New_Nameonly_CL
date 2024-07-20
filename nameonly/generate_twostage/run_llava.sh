90b1d65feea4f132c71566d2bd24ae336c7ee8aa
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-07-20 21:36:53 +0900

    Add: llava dataset

---
+++ nameonly/generate_twostage/run_llava.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 10개에 2시간 40분 -> 160분 -> 20개는 320분, 6시간 충분, 40개는 640분, 13시간 충분
+# 25개는 
+
+# Floyd - 4090: 1분에 2.2857개 / gpu, 0.4375분 / 1개
+#SBATCH -p suma_rtx4090
+#SBATCH --gres=gpu:1
+
+source ~/.bashrc
+ml purge
+
+conda init bash
+conda activate generate
+python generate_images_llava.py -r ./generated_datasets/llava -s 5 -e 10
