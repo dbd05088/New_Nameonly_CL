17d4fb5047fa3b4cf71b23cf6780fdd76419adf2
cryingInitial <specific0924@gmail.com>
2024-08-17 23:02:45 +0900

    Feat: HOI model selector

--- nameonly/generate_twostage/run_openworld.sh
+++ nameonly/generate_twostage/run_openworld.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# 10개에 2시간 40분 -> 160분 -> 20개는 320분, 6시간 충분, 40개는 640분, 13시간 충분
-# 25개는 
 
 # 4090
 # Floyd: 2.2857/min, 0.4375min/1 image ? 2/min??
@@ -16,4 +14,6 @@ ml purge
 
 conda init bash
 conda activate generate
-python generate_images_openworld.py -m sdturbo -r ./generated_datasets/openworld_diversified_sdturbo -s 305 -e 609 # 610개
+
+# OpenWorld Range: 0 ~ 609
+python generate_images_openworld.py -m sdturbo -r ./generated_datasets/openworld_diversified_sdturbo -s 305 -e 609
