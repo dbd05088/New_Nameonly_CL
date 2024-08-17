46fa43eb98c1cd72b69ad714f92f75179d7cac82
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-08-17 11:33:51 +0900

    Add: cogview geneation datacenter

--- nameonly/generate_twostage/run_openworld.sh
+++ nameonly/generate_twostage/run_openworld.sh
@@ -2,7 +2,12 @@
 # 10개에 2시간 40분 -> 160분 -> 20개는 320분, 6시간 충분, 40개는 640분, 13시간 충분
 # 25개는 
 
-# Floyd - 4090: 1분에 2.2857개 / gpu, 0.4375분 / 1개
+# 4090
+# Floyd: 2.2857/min, 0.4375min/1 image ? 2/min??
+# SDXL: 11/min, 0.0909min/1 image
+# sdturbo: fast
+# cogview2: 4/min, 0.25min/1 image
+
 #SBATCH -p suma_rtx4090
 #SBATCH --gres=gpu:1
 
@@ -11,4 +16,4 @@ ml purge
 
 conda init bash
 conda activate generate
-python generate_images_openworld.py -m sdxl -r ./generated_datasets/llava_openworld -s 0 -e 5
+python generate_images_openworld.py -m sdturbo -r ./generated_datasets/openworld_diversified_sdturbo -s 305 -e 609 # 610개
