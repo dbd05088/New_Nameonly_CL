7e598ac5e7beddcbba34b8900a9a9f12ac8e40a2
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-08-28 14:08:26 +0900

    Fix: hoi

--- nameonly/generate_twostage/run_hoi.sh
+++ nameonly/generate_twostage/run_hoi.sh
@@ -11,11 +11,17 @@
 
 source ~/.bashrc
 ml purge
-
 conda init bash
 conda activate generate
 
 # HOI Range: 0 ~ 227
 # 207, 165, 124, 151, 225 indices contain many images
 # Average number of images per class: 17 ~ 40
-python generate_images_hoi.py -m floyd -r ./generated_datasets/hoi_floyd -s 204 -e 227
+IMAGE_DIR='./generated_datasets/hoi_diversified_sdxl'
+GENERATIVE_MODEL="sdxl" # sdxl, floyd, cogview2, sd3, sdturbo, flux, kolors, auraflow
+START_CLASS=0
+END_CLASS=227
+PROMPT_DIR='../prompt_generation/prompts/hoi_diversified.json'
+
+python generate_images_hoi_diversified.py -m $GENERATIVE_MODEL -r $IMAGE_DIR -s $START_CLASS -e $END_CLASS -p $PROMPT_DIR
+
