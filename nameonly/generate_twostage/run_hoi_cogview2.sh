276fc79bfc49454254eb123ee18ecbb151022785
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-08-28 15:19:50 +0900

    Fix: cogview generation

--- nameonly/generate_twostage/run_hoi_cogview2.sh
+++ nameonly/generate_twostage/run_hoi_cogview2.sh
@@ -5,13 +5,16 @@
 # SDXL: 11/min, 0.0909min/1 image
 # sdturbo: fast
 # cogview2: 4/min, 0.25min/1 image
+# flux: 1.5/min???, 0.25min/1 image
+# kolors: 8.57/min, 0.1166min/1 image
+# auraflow: 1.5/min, 0.6667min/1 image
 
 #SBATCH -p suma_a6000
 ##SBATCH -q big_qos
 #SBATCH --gres=gpu:1
+##SBATCH --exclude=node[29]
 source ~/.bashrc
 ml purge
-
 conda init bash
 conda activate cogview
 
@@ -21,7 +24,13 @@ cd ../
 # HOI Range: 0 ~ 227
 # 207, 165, 124, 151, 225 indices contain many images
 # Average number of images per class: 17 ~ 40
-CUDA_HOME=/opt/ohpc/pub/apps/cuda/12.5 python generate_images_hoi.py -m cogview2 -r ./generated_datasets/hoi_cogview2 -s 0 -e 28
+IMAGE_DIR='./generated_datasets/hoi_diversified_cogview2'
+GENERATIVE_MODEL="cogview2" # sdxl, floyd, cogview2, sd3, sdturbo, flux, kolors, auraflow
+START_CLASS=0
+END_CLASS=0
+PROMPT_DIR='../prompt_generation/prompts/hoi_diversified.json'
+
+CUDA_HOME=/opt/ohpc/pub/apps/cuda/12.5 python generate_images_hoi_diversified.py -m $GENERATIVE_MODEL -r $IMAGE_DIR -s $START_CLASS -e $END_CLASS -p $PROMPT_DIR
 
 
 
