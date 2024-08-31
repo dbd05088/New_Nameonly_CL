c12dc7b1b6d7f7dad9a315473d2847bb7c7a26e5
Dongjae0324 <reccos1020@gmail.com>
2024-08-31 10:52:36 +0900

    Fix: requirements

--- new_requirements.sh
+++ new_requirements.sh
@@ -2,7 +2,7 @@
 
 pip install tensorboard
 pip install pandas
-pip install kornia
+pip install kornia==0.7.1
 pip install opencv-contrib-python
 pip install torch-optimizer
 pip install easydict
@@ -11,10 +11,12 @@ pip install timm
 pip install faiss-cpu==1.8.0
 pip install ftfy
 pip install regex
-pip install transformers
+pip install transformers==4.40.2
+pip install bitsandbytes
+pip install pycocoevalcap
 pip install peft
 pip install diffusers
 pip install scipy
-apt update
-apt install -y libgl1-mesa-glx
-apt install -y libglib2.0-0
+# apt update
+# apt install -y libgl1-mesa-glx
+# apt install -y libglib2.0-0
