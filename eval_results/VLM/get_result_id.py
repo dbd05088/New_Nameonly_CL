41edeb045263ff4b073723532d8df686528a8f4c
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-09-09 16:48:29 +0900

    result

--- eval_results/VLM/get_result_id.py
+++ eval_results/VLM/get_result_id.py
@@ -2,9 +2,13 @@ import os
 import numpy as np
 
 exp_dirs = os.listdir(".")
-seeds = [1,2,3]
+exp_dirs.sort()
+seeds = [2,3,4]
+target_dir = ['final_ver2_Bongard_HOI_ma_num7_iter0.5_meminf_bs4', 'final_ver2_Bongard_HOI_generated_num7_iter0.5_meminf_bs4', 'final_ver2_Bongard_HOI_generated_LLM_sdxl_ver3_num7_iter0.5_meminf_bs4', 'final_ver2_Bongard_HOI_generated_LLM_sdxl_ver2_num7_iter0.5_meminf_bs4', 'final_ver2_web_num7_iter0.5_meminf_bs4'] #'real_final_Bongard_HOI_generated_num7_iter0.5_meminf_bs4', 'real_final_Bongard_HOI_generated_diversified_new_sdxl_num7_iter0.5_meminf_bs4', 'Bongard_HOI_ma_num7_iter0.5_meminf_bs4', 'Bongard_HOI_web_num7_iter0.5_meminf_bs4', 'final_Bongard_HOI_ma_new_num7_iter0.5_meminf_bs4', 'final_Bongard_HOI_generated_diversified_new_sdxl_num7_iter0.5_meminf_bs4', 'Bongard_HOI_generated_num7_iter0.5_meminf_bs4', 'final_Bongard_HOI_generated_num7_iter0.5_meminf_bs4', 'final_Bongard_HOI_generated_RMD_new_num7_iter0.5_meminf_bs4', 'final_Bongard_HOI_generated_web_new_num7_iter0.5_meminf_bs4', 'Bongard_HOI_generated_RMD_new_num7_iter0.5_meminf_bs4'
 print(f"{'A_avg':>62} \t\t\t A_last")
 for exp_dir in exp_dirs:
+    if exp_dir not in target_dir:
+        continue
     last_accuracy = []
     average_accuracy = []
     for seed in seeds:
