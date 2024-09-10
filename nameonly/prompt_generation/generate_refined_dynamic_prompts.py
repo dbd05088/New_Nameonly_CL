d560d7668d9ae9ad0966c706b1a81aeffceee020
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-09-10 23:52:24 +0900

    Add: refined prompts

--- nameonly/prompt_generation/generate_refined_dynamic_prompts.py
+++ nameonly/prompt_generation/generate_refined_dynamic_prompts.py
@@ -1,22 +1,43 @@
 import re
+import os
 import json
 import nltk
 import random
 from classes import *
 from openai import OpenAI
 from tqdm import tqdm
+from copy import deepcopy
 
 random.seed(42)
 
 # important
-raw_prompt_json_path = './prompts/gpt4_hierarchy_cot_4_PACS.json' # this should be dynamic
-refined_prompt_json_path = './prompts/gpt4_hierarchy_cot_4_PACS_refined.json'
-dataset_count = PACS_count
-# important
+static_prompt_json_path = './prompts/gpt4_hierarchy_cot_4.json'
+refined_prompt_json_path = './prompts/gpt4_hierarchy_cot_4_DomainNet_refined.json'
+dataset_count = DomainNet_count
 
-def generate_refined_caption(client, cls, caption):
+# Load static prompt
+with open(static_prompt_json_path, 'r') as f:
+    static_prompt = json.load(f)
+
+# Convert static prompt to dynamic prompt
+dynamic_prompt_dict = {}
+for cls in dataset_count:
+    dynamic_prompt_dict[cls] = {'metaprompts': deepcopy(static_prompt['metaprompts'])}
+
+for k, v in dynamic_prompt_dict.items():
+    metaprompt_list = v['metaprompts']
+    for metaprompt_dict in metaprompt_list:
+        if '[concept]' in metaprompt_dict['metaprompt']:
+            metaprompt_dict['metaprompt'] = metaprompt_dict['metaprompt'].replace("[concept]", k)
+
+        for prompt_dict in metaprompt_dict['prompts']:
+            assert "[concept]" in prompt_dict['content'], f"No [concept] in {prompt_dict}"
+            prompt_dict['content'] = prompt_dict['content'].replace("[concept]", k.replace('_', ' '))
+
+
+def generate_refined_caption(client, concept, prompt):
     cls_tmp = cls.replace('_',' ')
-    base_message = f"To generate images using a text-to-image generation model, I've generated prompts. However, some sentences aren't appropriate for the concept, so I want to refine them into sentences that contain context suitable and reasonable for this concept. Both appropriate and inappropriate captions will be provided as input. Your caption is {caption}, and your concept is {cls_tmp} Output one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
+    base_message = f"To generate an image using a text-to-generation model, I created a prompt based on the concept. However, some sentences have contexts that do not fit the concept, so I would like to improve them into sentences with appropriate contexts. For example, when given the sentence 'A photo of a whale in the football field,' this has a context that does not fit the concept of a football field. This can be improved to 'A photo of a whale in the ocean.' When given a concept and the corresponding prompt, please refine it into a sentence with an appropriate context.\nconcept:{concept}\nprompt:{prompt}\nOutput one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
     
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -32,33 +53,26 @@ def generate_refined_caption(client, cls, caption):
 
 client = OpenAI(api_key="sk-proj-bPJxpKwauBBFBZJw7nEgT3BlbkFJePaQfARB48iyTbZfxSXg")
 
-with open(raw_prompt_json_path, 'r') as file:
-    data = json.load(file)
+# Open refined prompt json file
+if os.path.exists(refined_prompt_json_path):
+    with open(refined_prompt_json_path, 'r') as f:
+        totalprompt_dict = json.load(f)
+else:
+    totalprompt_dict = {}
 
-totalprompt_dict = {}
-for cls in tqdm(dataset_count):
-    totalprompt_dict[cls] = {'metaprompts': []}
-    class_prompt_list = []
-    
-    idx = 0
-    for i in range(10):
-        for j in range(5):            
-            caption_old = data[cls]['metaprompts'][i]['prompts'][j]['content']
-            caption_new = generate_refined_caption(client, cls, caption_old)
+for cls, cls_prompt_dict in tqdm(dynamic_prompt_dict.items()):
+    if cls in totalprompt_dict:
+        print(f"Skipping {cls}")
+        continue
+        
+    cls_prompt_list = cls_prompt_dict['metaprompts']
+    for metaprompt_dict in tqdm(cls_prompt_list):
+        for prompt_dict in metaprompt_dict['prompts']:
+            print(f"Before: {prompt_dict['content']}")
+            new_prompt = generate_refined_caption(client, cls, prompt_dict['content'])
+            prompt_dict['content'] = new_prompt
+            print(f"After: {prompt_dict['content']}\n")
             
-            totalprompt_dict[cls]['metaprompts'].append(
-                {
-                'index': idx,
-                'metaprompt': 'dummy',
-                'prompts': [
-                    {
-                        'index': 0,
-                        'content': caption_new
-                    }
-                ]
-            }
-            )
-            idx+=1
-
-with open(refined_prompt_json_path, 'w') as f:
-    json.dump(totalprompt_dict, f)
+    totalprompt_dict[cls] = cls_prompt_dict
+    with open(refined_prompt_json_path, 'w') as f:
+        json.dump(totalprompt_dict, f)
