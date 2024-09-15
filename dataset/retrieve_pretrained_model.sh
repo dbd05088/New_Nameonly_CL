054e9a9ccfb9196c44781e8c7df7e0685231e5b1
d <d>
2024-09-15 11:47:27 +0900

    fix pretrained

---
+++ dataset/retrieve_pretrained_model.sh
@@ -0,0 +1,9 @@
+./gdrive account remove dbd05088@naver.com
+./gdrive account import gdrive_export-dbd05088_naver_com.tar
+./gdrive account switch dbd05088@naver.com
+
+./gdrive files download 1e5pzt5L8EKEPbyYt91wuwggXolT9Xw3N
+
+tar -xvf RnD_ckpt.tar
+mv RnD_ckpt ../
+rm RnD_ckpt.tar
