76653bc78e24a547bef061582f31575666e3c880
HyeonbeomChoi <gusqja1228@naver.com>
2024-09-16 15:06:31 +0900

    Add: new types for prompt refinement

--- nameonly/prompt_generation/generate_refined_dynamic_prompts.py
+++ nameonly/prompt_generation/generate_refined_dynamic_prompts.py
@@ -1,18 +1,23 @@
 import re
 import os
 import json
-import nltk
 import random
 from classes import *
 from openai import OpenAI
 from tqdm import tqdm
 from copy import deepcopy
 
+# original, new1, new2
+# base_message = f"To generate an image using a text-to-generation model, I created a prompt based on the concept. However, some sentences have contexts that do not fit the concept, so I would like to improve them into sentences with appropriate contexts. For example, when given the sentence 'A photo of a whale in the football field,' this has a context that does not fit the concept of a football field. This can be improved to 'A photo of a whale in the ocean.' When given a concept and the corresponding prompt, please refine it into a sentence with an appropriate context.\nconcept:{concept}\nprompt:{prompt}\nOutput one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
+# base_message = f"To generate an image using a text-to-generation model, I created a prompt based on the concept. However, some sentences have contexts that do not fit the concept, so I would like to improve them into sentences with appropriate contexts. For example, when given the sentence 'A sunset beach scene with an elephant in warm, pastel colors.' this has a context(beach) that does not fit the concept of an elephant. This can be improved to 'A sunset savanna scene with an elephant in warm, pastel colors.' When given a concept and the corresponding prompt, please refine it into a sentence with an appropriate context.\nconcept:{concept_tmp}\nprompt:{prompt}\nOutput one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
+# base_message = f"To generate an image using a text-to-generation model, I created a prompt based on the concept. However, some sentences have contexts that do not fit the concept, so I would like to improve them into sentences with appropriate contexts. For example, when given the sentence 'A surreal underwater world featuring giraffes in pastel colors.' this has a context(underwater world) that does not fit the concept of a giraffe. This can be improved to 'A surreal savanna landscape featuring giraffes in pastel colors.' When given a concept and the corresponding prompt, please refine it into a sentence with an appropriate context.\nconcept:{concept_tmp}\nprompt:{prompt}\nOutput one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
+
 random.seed(42)
 
 def generate_refined_caption(client, concept, prompt):
-    cls_tmp = cls.replace('_',' ')
-    base_message = f"To generate an image using a text-to-generation model, I created a prompt based on the concept. However, some sentences have contexts that do not fit the concept, so I would like to improve them into sentences with appropriate contexts. For example, when given the sentence 'A photo of a whale in the football field,' this has a context that does not fit the concept of a football field. This can be improved to 'A photo of a whale in the ocean.' When given a concept and the corresponding prompt, please refine it into a sentence with an appropriate context.\nconcept:{concept}\nprompt:{prompt}\nOutput one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
+    concept_tmp = concept.replace('_',' ')
+    # new2
+    base_message = f"To generate an image using a text-to-generation model, I created a prompt based on the concept. However, some sentences have contexts that do not fit the concept, so I would like to improve them into sentences with appropriate contexts. For example, when given the sentence 'A surreal underwater world featuring giraffes in pastel colors.' this has a context(underwater world) that does not fit the concept of a giraffe. This can be improved to 'A surreal savanna landscape featuring giraffes in pastel colors.' When given a concept and the corresponding prompt, please refine it into a sentence with an appropriate context.\nconcept:{concept_tmp}\nprompt:{prompt}\nOutput one single grammatically correct caption with refinement. Do not output any notes, word counts, facts, etc. Output one single sentence only."
     
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -29,9 +34,10 @@ def generate_refined_caption(client, concept, prompt):
 
 client = OpenAI(api_key="sk-proj-bPJxpKwauBBFBZJw7nEgT3BlbkFJePaQfARB48iyTbZfxSXg")
 # important
-static_prompt_json_path = './prompts/gpt4_hierarchy_cot_50_2.json'
-refined_prompt_json_path = './prompts/gpt4_hierarchy_cot_50_2_refined_PACS.json'
+static_prompt_json_path = './prompts/gpt4_hierarchy_cot_100_2.json'
+refined_prompt_json_path = './prompts/gpt4_hierarchy_cot_100_2_refined_PACS_new2.json'
 dataset_count = PACS_count
+# important
 
 # Load static prompt
 with open(static_prompt_json_path, 'r') as f:
