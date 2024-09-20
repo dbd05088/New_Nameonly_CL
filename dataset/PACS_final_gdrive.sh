09574cc10a8e44e1e11bd8f70077943a54966bac
HyeonbeomChoi <gusqja1228@naver.com>
2024-09-20 16:30:35 +0900

    Add: PACS base_sdxl

--- dataset/PACS_final_gdrive.sh
+++ dataset/PACS_final_gdrive.sh
@@ -973,22 +973,27 @@
 # rm PACS_final_wo_cot_wo_hierarhcy_50_sdxl_floyd_cogview2_sd3_flux_auraflow.tar
 # rm PACS_final_wo_cot_wo_hierarhcy_50_sdxl_floyd_cogview2_sd3_auraflow.tar
 
-# 6 web_DINO_base (0919)
-./gdrive files download 10JeVMxs-THLiTx5imPZqfoepXKuBFh01 # PACS_final_web_DINO_base_Adacore_30_0.0001
-./gdrive files download 19d_YV0awo9Ye3ZU76XuxPn5xojHGBRfM # PACS_final_web_DINO_base_CurvMatch_30_0.0001
-./gdrive files download 1Ud_w1HaZ-E-MQwztUcivo87p_WYdgDAy # PACS_final_web_DINO_base_Glister_30_0.0001
-./gdrive files download 1gk79XmbxZaqDSnln3FEBW2Ko4r5q7yLa # PACS_final_web_DINO_base_GradMatch_30_0.0001
-./gdrive files download 1R7r7fq8vfRwCVbWD5i7hkKDuS2zavB-E # PACS_final_web_DINO_base_Submodular_30_0.0001
-./gdrive files download 1q-RK9SOW-oc-IhzK0Ce7jeEY-apiijcL # PACS_final_web_DINO_base_Uncertainty_30_0.0001
-tar -xf PACS_final_web_DINO_base_Adacore_30_0.0001.tar -C PACS_final
-tar -xf PACS_final_web_DINO_base_CurvMatch_30_0.0001.tar -C PACS_final
-tar -xf PACS_final_web_DINO_base_Glister_30_0.0001.tar -C PACS_final
-tar -xf PACS_final_web_DINO_base_GradMatch_30_0.0001.tar -C PACS_final
-tar -xf PACS_final_web_DINO_base_Submodular_30_0.0001.tar -C PACS_final
-tar -xf PACS_final_web_DINO_base_Uncertainty_30_0.0001.tar -C PACS_final
-rm PACS_final_web_DINO_base_Adacore_30_0.0001.tar
-rm PACS_final_web_DINO_base_CurvMatch_30_0.0001.tar
-rm PACS_final_web_DINO_base_Glister_30_0.0001.tar
-rm PACS_final_web_DINO_base_GradMatch_30_0.0001.tar
-rm PACS_final_web_DINO_base_Submodular_30_0.0001.tar
-rm PACS_final_web_DINO_base_Uncertainty_30_0.0001.tar
+# # 6 web_DINO_base (0919)
+# ./gdrive files download 10JeVMxs-THLiTx5imPZqfoepXKuBFh01 # PACS_final_web_DINO_base_Adacore_30_0.0001
+# ./gdrive files download 19d_YV0awo9Ye3ZU76XuxPn5xojHGBRfM # PACS_final_web_DINO_base_CurvMatch_30_0.0001
+# ./gdrive files download 1Ud_w1HaZ-E-MQwztUcivo87p_WYdgDAy # PACS_final_web_DINO_base_Glister_30_0.0001
+# ./gdrive files download 1gk79XmbxZaqDSnln3FEBW2Ko4r5q7yLa # PACS_final_web_DINO_base_GradMatch_30_0.0001
+# ./gdrive files download 1R7r7fq8vfRwCVbWD5i7hkKDuS2zavB-E # PACS_final_web_DINO_base_Submodular_30_0.0001
+# ./gdrive files download 1q-RK9SOW-oc-IhzK0Ce7jeEY-apiijcL # PACS_final_web_DINO_base_Uncertainty_30_0.0001
+# tar -xf PACS_final_web_DINO_base_Adacore_30_0.0001.tar -C PACS_final
+# tar -xf PACS_final_web_DINO_base_CurvMatch_30_0.0001.tar -C PACS_final
+# tar -xf PACS_final_web_DINO_base_Glister_30_0.0001.tar -C PACS_final
+# tar -xf PACS_final_web_DINO_base_GradMatch_30_0.0001.tar -C PACS_final
+# tar -xf PACS_final_web_DINO_base_Submodular_30_0.0001.tar -C PACS_final
+# tar -xf PACS_final_web_DINO_base_Uncertainty_30_0.0001.tar -C PACS_final
+# rm PACS_final_web_DINO_base_Adacore_30_0.0001.tar
+# rm PACS_final_web_DINO_base_CurvMatch_30_0.0001.tar
+# rm PACS_final_web_DINO_base_Glister_30_0.0001.tar
+# rm PACS_final_web_DINO_base_GradMatch_30_0.0001.tar
+# rm PACS_final_web_DINO_base_Submodular_30_0.0001.tar
+# rm PACS_final_web_DINO_base_Uncertainty_30_0.0001.tar
+
+# base_sdxl (0920)
+./gdrive files download 1NCJ-UYhwAvsEIfyZqCf6qd8867dnW7bH # PACS_final_base_sdxl
+tar -xf PACS_final_base_sdxl.tar -C PACS_final
+rm PACS_final_base_sdxl.tar
