e0f891f2f02c8804da17054916bd063412b957e4
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-09-30 16:30:02 +0900

    Add: quali prompt list

--- nameonly/prompt_generation/nested_prompt_to_list.py
+++ nameonly/prompt_generation/nested_prompt_to_list.py
@@ -2,9 +2,9 @@
 import json
 from classes import *
 
-prompt_path = './prompts/gpt4_hierarchy_cot_50_2.json'
-output_path = './diversity_prompts/gpt4_hierarchy_cot_50_2_DomainNet.json'
-count_dict = DomainNet_count
+prompt_path = './prompts/LE_PACS.json'
+output_path = './quali_prompts/LE_PACS_list.json'
+count_dict = PACS_count
 
 with open(prompt_path, 'r') as f:
     data_dict = json.load(f)
