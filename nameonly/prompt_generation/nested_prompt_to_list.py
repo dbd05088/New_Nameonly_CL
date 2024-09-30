9aab7406b3978439436acbe47b112f428802afb2
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-09-30 17:11:09 +0900

    Add: LE DomainNet list

--- nameonly/prompt_generation/nested_prompt_to_list.py
+++ nameonly/prompt_generation/nested_prompt_to_list.py
@@ -2,9 +2,9 @@
 import json
 from classes import *
 
-prompt_path = './prompts/LE_PACS.json'
-output_path = './quali_prompts/LE_PACS_list.json'
-count_dict = PACS_count
+prompt_path = './prompts/LE_DomainNet.json'
+output_path = './quali_prompts/LE_DomainNet_list.json'
+count_dict = DomainNet_count
 
 with open(prompt_path, 'r') as f:
     data_dict = json.load(f)
