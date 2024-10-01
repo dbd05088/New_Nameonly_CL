dcb1e0c5aa44b727b6a6c72cc5bd19881a386ed4
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-10-01 14:17:09 +0900

    Quali prompts

--- nameonly/prompt_generation/static_to_dynamic.py
+++ nameonly/prompt_generation/static_to_dynamic.py
@@ -6,9 +6,9 @@ from classes import *
 # static prompt는 class name이 없는데, image generation code는 class - prompt dict로
 # 되어 있기 때문에 이를 수정하는 부분.
 
-classes = list(PACS_count.keys())
-static_path = './prompts/gpt4_wo_hierarchy_wo_cot.json'
-output_path = './prompts/gpt4_wo_hierarchy_wo_cot_PACS.json'
+classes = list(cifar10_count.keys())
+static_path = './prompts/gpt4_hierarchy_cot_50_2.json'
+output_path = './quali_prompts/gpt4_hierarchy_cot_50_2_cifar10.json'
 
 with open(static_path, 'r') as f:
     static_dict = json.load(f)
@@ -26,7 +26,7 @@ for k, v in static_to_dynamic_dict.items():
 
         for prompt_dict in metaprompt_dict['prompts']:
             assert "[concept]" in prompt_dict['content'], f"No [concept] in {prompt_dict}"
-            prompt_dict['content'] = prompt_dict['content'].replace("[concept]", k)
+            prompt_dict['content'] = prompt_dict['content'].replace("[concept]", k.replace('_', ' '))
 
 with open(output_path, 'w') as f:
     json.dump(static_to_dynamic_dict, f)
