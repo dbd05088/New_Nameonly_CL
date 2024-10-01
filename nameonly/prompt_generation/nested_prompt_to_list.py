f070ee4924c4ba0d134f883902d3518e4be4eb8d
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-10-01 14:17:09 +0900

    Quali prompts

--- nameonly/prompt_generation/nested_prompt_to_list.py
+++ nameonly/prompt_generation/nested_prompt_to_list.py
@@ -2,9 +2,9 @@
 import json
 from classes import *
 
-prompt_path = './prompts/LE_DomainNet.json'
-output_path = './quali_prompts/LE_DomainNet_list.json'
-count_dict = DomainNet_count
+prompt_path = './prompts/synthclip_cifar10.json'
+output_path = './quali_prompts/synthclip_cifar10_list.json'
+count_dict = cifar10_count
 
 with open(prompt_path, 'r') as f:
     data_dict = json.load(f)
