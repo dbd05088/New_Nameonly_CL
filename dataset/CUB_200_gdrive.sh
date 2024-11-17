dd9c7adbc649d4bb9f2812c2aad3348c88f30607
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-11-17 14:28:12 +0900

    Fix: CUB_200 gdrive

--- dataset/CUB_200_gdrive.sh
+++ dataset/CUB_200_gdrive.sh
@@ -1,22 +1,22 @@
-# ./gdrive account remove dbd05088@naver.com
-# ./gdrive account import gdrive_export-dbd05088_naver_com.tar
-# ./gdrive account switch dbd05088@naver.com
+./gdrive account remove dbd05088@naver.com
+./gdrive account import gdrive_export-dbd05088_naver_com.tar
+./gdrive account switch dbd05088@naver.com
 
 
-# # Required
-# mkdir -p CUB_200
-# ./gdrive files download 1VYNnUu5F8InA-Vx2EzU6JBxJzpzDdoqr # test_ma
-# ./gdrive files download 1nrXm3X5lpQw7skouKVwOhtJWEFz6zbWJ # painting
+# Required
+mkdir -p CUB_200
+./gdrive files download 1VYNnUu5F8InA-Vx2EzU6JBxJzpzDdoqr # test_ma
+./gdrive files download 1nrXm3X5lpQw7skouKVwOhtJWEFz6zbWJ # painting
 
-# tar -xf CUB_200_test_ma.tar -C CUB_200
-# tar -xf CUB_200_painting.tar -C CUB_200
-# rm CUB_200_test_ma.tar
-# rm CUB_200_painting.tar
+tar -xf CUB_200_test_ma.tar -C CUB_200
+tar -xf CUB_200_painting.tar -C CUB_200
+rm CUB_200_test_ma.tar
+rm CUB_200_painting.tar
 
-# # train_ma
-# ./gdrive files download 1dVqhry8jsg7SJh0nEt-uSkOgahyUKGeN
-# tar -xf CUB_200_train_ma.tar -C CUB_200
-# rm CUB_200_train_ma.tar
+# train_ma
+./gdrive files download 1dVqhry8jsg7SJh0nEt-uSkOgahyUKGeN
+tar -xf CUB_200_train_ma.tar -C CUB_200
+rm CUB_200_train_ma.tar
 
 # sdxl, RMD
 ./gdrive files download 10oIDXVvCwUFERltPqAslBQMuVU4yZHku # CUB_200_50_2_sdxl
