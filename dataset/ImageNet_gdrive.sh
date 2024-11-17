1abb7e3b792cd7f9e71d2496af4181da3b0a1ce4
seongwon980 <poiuy98749@yonsei.ac.kr>
2024-11-18 03:41:42 +0900

    add imagenet eval

--- dataset/ImageNet_gdrive.sh
+++ dataset/ImageNet_gdrive.sh
@@ -5,15 +5,22 @@
 
 # # Required
 # mkdir -p ImageNet
-# ./gdrive files download 1mM5xy9nkuvWY1Bv2-VLnfNO2zXYvExV_ # ImageNet_c
+## ./gdrive files download 1mM5xy9nkuvWY1Bv2-VLnfNO2zXYvExV_ # ImageNet_c
 # ./gdrive files download 11XEooWsl5SSL4XgiXvDnDNOsQiz_Ebjb # ImageNet_r_50
 # ./gdrive files download 1NLRNn8HeC41JfidifkwUwL2OKKAH4ovB # ImageNet_test
-# tar -xf ImageNet_c.tar -C ImageNet
+# ./gdrive files download 13VQ678_Cs2qnWLULQR4Vp5cK2XV4j1n5 # ImageNet_c_all
+# ./gdrive files download 1P23z_1FNJhyYS82eWVwnREIRkkkPuRIB # ImageNet_d_new
+## tar -xf ImageNet_c.tar -C ImageNet
 # tar -xf ImageNet_r_50.tar -C ImageNet
 # tar -xf ImageNet_test.tar -C ImageNet
-# rm ImageNet_c.tar
+# tar -xf ImageNet_c_all.tar -C ImageNet
+# tar -xf ImageNet_d_new.tar -C ImageNet
+## rm ImageNet_c.tar
 # rm ImageNet_r_50.tar
 # rm ImageNet_test.tar
+# rm ImageNet_c_all.tar
+# rm ImageNet_d_new.tar
+
 
 # # ImageNet RMD
 # ./gdrive files download 120YZogsTblFW0Li-hbYg2uMMCSuOB-2k # ImageNet_50_2_sdxl_floyd_cogview2_sd3_auraflow
