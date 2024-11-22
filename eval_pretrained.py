1698cc4a49acbef9b2b0fb741e6a3c1a6c7ba6a8
ta3h30nk1m <ta3h30nk1m@gmail.com>
2024-11-22 15:18:53 +0900

    add pretrain eval

---
+++ eval_pretrained.py
@@ -0,0 +1,216 @@
+import logging.config
+import os
+import random
+from collections import defaultdict
+import numpy as np
+import torch
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from torch.utils.tensorboard import SummaryWriter
+
+from configuration import config
+from utils.data_loader import get_test_datalist
+from utils.data_loader import get_train_datalist
+from utils.method_manager_new import select_method
+from utils.train_utils import send_message
+
+def load_trained_item(model, ckpt_path):
+    print("CKPT PATH", ckpt_path)
+    ckpt = torch.load(ckpt_path)
+    print("Loaded checkpoint")
+    model.load_state_dict(ckpt['model_state_dict'])
+    seen_tasks = ckpt.get('seen_tasks', [])
+    return model, ckpt['num_samples'], seen_tasks
+
+def main():
+    args = config.base_parser()
+    #num_samples = {'cifar10': 10000, 'PACS':1670, 'cifar10_10': 10000, 'PACS_10':16700, "OfficeHome":4357, "DomainNet":50872}
+    logging.config.fileConfig("./configuration/logging.conf")
+    logger = logging.getLogger()
+
+    os.makedirs(f"results/{args.dataset}/{args.note}/{args.type_name}", exist_ok=True)
+    # os.makedirs(f"tensorboard/{args.dataset}/{args.note}/{args.type_name}", exist_ok=True)
+    fileHandler = logging.FileHandler(f'results/{args.dataset}/{args.note}/{args.type_name}/seed_{args.rnd_seed}.log', mode="w")
+
+    formatter = logging.Formatter(
+        "[%(levelname)s] %(filename)s:%(lineno)d > %(message)s"
+    )
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+
+    #writer = SummaryWriter(f'tensorboard/{args.dataset}/{args.note}/seed_{args.rnd_seed}')
+
+    logger.info(args)
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    logger.info(f"Set the device ({device})")
+
+    # Fix the random seeds
+    torch.manual_seed(args.rnd_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(args.rnd_seed)
+    random.seed(args.rnd_seed)
+
+
+    # get datalist
+    # train_datalist, cls_dict, cls_addition = get_train_datalist(args.dataset, args.sigma, args.repeat, args.init_cls, args.rnd_seed, args.type_name)
+    test_domain_name, test_datalists = get_test_datalist(args.dataset)
+    samples_cnt = 0
+    # print("args.dataset", args.dataset, "num_samples", len(train_datalist)) #num_samples[args.dataset]
+
+    # Reduce datalist in Debug mode
+    # if args.debug:
+    #     random.shuffle(train_datalist)
+    #     train_datalist = train_datalist[:5000]
+    #     random.shuffle(test_datalist)
+    #     test_datalist = test_datalist[:2000]
+
+
+    eval_point = [int(point) for point in args.eval_point.split(" ")]
+        
+    print("eval_point")
+    print(eval_point)
+    
+    logger.info(f"Select a CIL method ({args.mode})")
+    method = select_method(args, [], test_datalists, device)
+
+    print("\n###flops###\n")
+    #method.get_flops_parameter()
+
+    eval_results = defaultdict(list)
+
+    samples_cnt = 0
+    task_id = 0
+    cur_task=0
+    
+    
+    eval_pretrain_weight_dir =  f"pretrained/{args.dataset}/{args.type_name}/{args.online_iter}_{args.rnd_seed}"
+    for weight in os.listdir(eval_pretrain_weight_dir):
+        weight_dir = os.path.join(eval_pretrain_weight_dir, weight)
+        model, seen_tasks = load_trained_item(method.model, weight_dir)
+        method.exposed_classes = seen_tasks
+        method.model = model
+        avg_acc = []
+        cls_acc = []
+        avg_loss = []
+        method.report_test("Total", sample_num, np.mean(avg_loss), np.mean(avg_acc), np.mean(cls_acc))
+        eval_results["test_acc"].append(np.mean(avg_acc))
+        eval_results["percls_acc"].append(np.mean(cls_acc))
+        eval_results["data_cnt"].append(samples_cnt)
+        for domain_name, test_datalist  in zip(test_domain_name, test_datalists):
+            sample_num, eval_dict = method.online_evaluate(domain_name, cur_task, test_datalist, samples_cnt, 32, args.n_worker, cls_dict, cls_addition, data["time"])
+            avg_acc.append(eval_dict['avg_acc'])
+            avg_loss.append(eval_dict['avg_loss'])
+            cls_acc.append(eval_dict['cls_acc'])
+        if samples_cnt in eval_point:
+            eval_results["avg_test_acc"].append(np.mean(avg_acc))
+            method.report_test("Task", sample_num, np.mean(avg_loss), np.mean(avg_acc), np.mean(cls_acc))
+            cur_task+=1
+    A_last = eval_results["test_acc"][-1] #eval_dict['avg_acc']
+    # Accuracy (A)
+    A_auc = np.mean(eval_results["test_acc"])
+    A_avg = np.mean(eval_results["avg_test_acc"])
+
+    # KLR_avg = np.mean(method.knowledge_loss_rate[1:])
+    # KGR_avg = np.mean(method.knowledge_gain_rate)
+    KLR_avg = 0.0
+    KGR_avg = 0.0
+
+    logger.info(f"======== Summary =======")
+    logger.info(f"A_auc {A_auc:6f} | A_last {A_last:6f} | A_avg {A_avg:6f} | KLR_avg {KLR_avg:6f} | KGR_avg {KGR_avg:6f} | Total FLOPs {method.total_flops:4f}")
+    
+    # for i, data in enumerate(train_datalist):
+    #     # explicit task boundary for twf
+    #     if samples_cnt in [0] + eval_point and args.mode in ["bic", "xder", "der_lider", "er_lider", "xder_lider", "co2l"]:
+    #         method.online_before_task(task_id)
+    #         task_id += 1
+        
+    #     samples_cnt += 1
+    #     method.online_step(data, samples_cnt, args.n_worker)
+    #     #if samples_cnt % args.eval_period == 0:
+    #     if samples_cnt in eval_point or samples_cnt % args.eval_period == 0:
+    #         avg_acc = []
+    #         cls_acc = []
+    #         avg_loss = []
+            
+    #         method.report_test("Total", sample_num, np.mean(avg_loss), np.mean(avg_acc), np.mean(cls_acc))
+    #         eval_results["test_acc"].append(np.mean(avg_acc))
+    #         eval_results["percls_acc"].append(np.mean(cls_acc))
+    #         eval_results["data_cnt"].append(samples_cnt)
+    #         if samples_cnt in eval_point:
+    #             eval_results["avg_test_acc"].append(np.mean(avg_acc))
+    #             method.report_test("Task", sample_num, np.mean(avg_loss), np.mean(avg_acc), np.mean(cls_acc))
+    #             cur_task+=1
+    #     if samples_cnt in eval_point and (args.mode in ["memo", "xder", "afec"]) and samples_cnt != len(train_datalist):
+    #         method.online_after_task()
+        
+    # if eval_results["data_cnt"][-1] != samples_cnt:
+    #     for domain_name, test_datalist  in zip(test_domain_name, test_datalists):
+    #         sample_num, eval_dict = method.online_evaluate(domain_name, test_datalist, samples_cnt, 32, args.n_worker, cls_dict, cls_addition, data["time"])
+    #         avg_acc.append(eval_dict['avg_acc'])
+    #         avg_loss.append(eval_dict['avg_loss'])
+    #         cls_acc.append(eval_dict['cls_acc'])
+    #     method.report_test("Total", sample_num, np.mean(avg_loss), np.mean(avg_acc), np.mean(cls_acc))
+    #     eval_results["test_acc"].append(np.mean(avg_acc))
+    #     eval_results["percls_acc"].append(np.mean(cls_acc))
+    #     eval_results["data_cnt"].append(samples_cnt)
+    #     if samples_cnt in eval_point:
+    #         eval_results["avg_test_acc"].append(np.mean(avg_acc))
+    #         method.report_test("Task", sample_num, np.mean(avg_loss), np.mean(avg_acc), np.mean(cls_acc))
+
+    # A_last = eval_results["test_acc"][-1] #eval_dict['avg_acc']
+
+    # if args.mode == 'gdumb':
+    #     eval_results = method.evaluate_all(test_datalist, args.memory_epoch, args.batchsize, args.n_worker, cls_dict, cls_addition)
+
+    # np.save(f'results/{args.dataset}/{args.note}/seed_{args.rnd_seed}_eval.npy', eval_results['test_acc'])
+    # np.save(f'results/{args.dataset}/{args.note}/seed_{args.rnd_seed}_eval_per_cls.npy', eval_results['percls_acc'])
+    # np.save(f'results/{args.dataset}/{args.note}/seed_{args.rnd_seed}_eval_time.npy', eval_results['data_cnt'])
+
+
+    # # Accuracy (A)
+    # A_auc = np.mean(eval_results["test_acc"])
+    # A_avg = np.mean(eval_results["avg_test_acc"])
+
+    # # KLR_avg = np.mean(method.knowledge_loss_rate[1:])
+    # # KGR_avg = np.mean(method.knowledge_gain_rate)
+    # KLR_avg = 0.0
+    # KGR_avg = 0.0
+
+    # logger.info(f"======== Summary =======")
+    # logger.info(f"A_auc {A_auc:6f} | A_last {A_last:6f} | A_avg {A_avg:6f} | KLR_avg {KLR_avg:6f} | KGR_avg {KGR_avg:6f} | Total FLOPs {method.total_flops:4f}")
+    # eval_root = f"results/{args.dataset}/{args.note}/{args.type_name}"
+    # if args.dataset == "DomainNet":
+    #     send_message("https://hooks.slack.com/services/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", f"{eval_root}_seed_{args.rnd_seed} complete!")
+    # eval_log_list = os.listdir(eval_root)
+    # seeds = (1, 2, 3, 4, 5)
+    # seed_results = [f"seed_{seed}.log" in eval_log_list for seed in seeds] # [T, T, T, T, T]
+    # if all(seed_results):
+    #     print(f"All seed logs are present")
+    #     summary_present_in_all = True
+
+    #     for seed in seeds:
+    #         log_path = os.path.join(eval_root, f"seed_{seed}.log")
+    #         with open(log_path, 'r') as file:
+    #             if 'Summary' not in file.read():
+    #                 summary_present_in_all = False
+    #                 print(f"'Summary' not found in seed_{seed}.log")
+    #                 break
+
+    #     if summary_present_in_all:
+    #         print("All seed evaluations are complete and successful.")
+    #         if args.dataset == "DomainNet":
+    #             send_message("https://hooks.slack.com/services/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", f"{eval_root}_seed_{args.rnd_seed} - All seed evaluations are complete and successful.")
+    #         new_folder_name = f"complete_{os.path.basename(eval_root)}"
+    #         new_path = os.path.join(os.path.dirname(eval_root), new_folder_name)
+    #         os.rename(eval_root, new_path)
+    #         print(f"Directory renamed to {new_folder_name}")
+            
+
+if __name__ == "__main__":
+    torch.multiprocessing.set_start_method('spawn')
+    main()
---
+++ eval_pretrained.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+#SBATCH -p suma_rtx4090
+##SBATCH -q big_qos
+#SBATCH --job-name=P_v_50_2_eq
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=node37
+##SBATCH -c 32
+
+# ulimit -u 200000
+# source ~/.bashrc
+# ml purge
+# conda init bash
+# conda activate generate
+
+# --------------------------IMPORTANT-------------------------- #
+MODE="er" # er, der, mir, aser, ...
+MODEL_NAME="resnet18" # vit
+DATASET="DomainNet" # PACS_final, DomainNet, cifar10, NICO, cct
+TYPES=("50_2_sdxl_floyd_cogview2_sd3_auraflow_0_0625") # each type runs on each gpu
+SEEDS="1"
+GPUS=("6" "7" "2" "3" "4" "5" "6" "7") # each gpu runs each type
+# --------------------------IMPORTANT-------------------------- #
+
+# If explicitly provided, use the provided arguments
+MODE=${1:-$MODE}
+MODEL_NAME=${2:-$MODEL_NAME}
+DATASET=${3:-$DATASET}
+if [ -n "$4" ]; then
+    TYPES=("$4")
+fi
+if [ -n "$5" ]; then
+    SEEDS="$5"
+fi
+NOTE="iclr_${MODEL_NAME}_${DATASET}_${MODE}"
+
+echo "MODE: $MODE"
+echo "MODEL_NAME: $MODEL_NAME"
+echo "DATASET: $DATASET"
+echo "TYPES: ${TYPES[@]}"
+echo "SEEDS: $SEEDS"
+
+TRANSFORM_ON_GPU="--transform_on_gpu"
+N_WORKER=4
+FUTURE_STEPS=8
+EVAL_N_WORKER=4
+EVAL_BATCH_SIZE=500
+#USE_KORNIA="--use_kornia"
+USE_KORNIA=""
+UNFREEZE_RATE=0.5
+DATA_DIR=""
+K_COEFF="4"
+TEMPERATURE="0.125"
+
+SIGMA=0
+REPEAT=1
+INIT_CLS=100
+USE_AMP=""
+
+#--no_eval_during_train
+
+if [ "$DATASET" == "cifar10" ]; then
+    MEM_SIZE=1000
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=500
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=6000 FEAT_DIM=14 FEAT_MEM_SIZE=24000
+    SAMPLES_PER_TASK=20000 
+    ONLINE_ITER=2
+    EVAL_POINT="2000 4000 6000 8000 10000"
+
+elif [ "$DATASET" == "PACS_final" ]; then
+    MEM_SIZE=200
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=100
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
+    SAMPLES_PER_TASK=2000
+    ONLINE_ITER=2
+
+    if [ "$SEEDS" == "1" ]; then
+        EVAL_POINT="458 764 1333"
+    elif [ "$SEEDS" == "2" ]; then
+        EVAL_POINT="638 948 1333"
+    elif [ "$SEEDS" == "3" ]; then
+        EVAL_POINT="520 1013 1333"
+    elif [ "$SEEDS" == "4" ]; then
+        EVAL_POINT="641 950 1333"
+    elif [ "$SEEDS" == "5" ]; then
+        EVAL_POINT="455 764 1333"
+    fi
+
+elif [ "$DATASET" == "cct" ]; then
+    MEM_SIZE=400
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=100
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=1002 FEAT_DIM=14 FEAT_MEM_SIZE=4800
+    SAMPLES_PER_TASK=2000
+    ONLINE_ITER=2
+    EVAL_POINT="600 1200 1800 2400"
+
+elif [ "$DATASET" == "DomainNet" ]; then
+    MEM_SIZE=10000
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=2000
+    BATCHSIZE=128; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
+    SAMPLES_PER_TASK=2000
+    ONLINE_ITER=3
+    if [ "$SEEDS" == "1" ]; then
+        EVAL_POINT="10037 20073 30338 40393 50273"
+    elif [ "$SEEDS" == "2" ]; then
+        EVAL_POINT="10189 20304 30413 40177 50273"
+    elif [ "$SEEDS" == "3" ]; then
+        EVAL_POINT="9825 19943 30186 40109 50273"
+    elif [ "$SEEDS" == "4" ]; then
+        EVAL_POINT="10276 20398 30423 40359 50273"
+    elif [ "$SEEDS" == "5" ]; then
+        EVAL_POINT="9918 20137 30290 40184 50273"
+    fi
+
+elif [ "$DATASET" == "NICO" ]; then
+    MEM_SIZE=8000 # (changed 0901 - after 10x increase)
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=2000
+    BATCHSIZE=64; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
+    SAMPLES_PER_TASK=960
+    ONLINE_ITER=3
+    EVAL_POINT="9600 19200 28800 38400 48000"
+
+elif [ "$DATASET" == "ImageNet_200" ]; then
+    MEM_SIZE=20000 # (changed 0901 - after 10x increase)
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=5000
+    BATCHSIZE=256; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
+    SAMPLES_PER_TASK=40000
+    ONLINE_ITER=0.25
+    EVAL_POINT="40000 80000 120000 160000 200000"
+
+elif [ "$DATASET" == "ImageNet_400" ]; then
+    MEM_SIZE=40000 # (changed 0901 - after 10x increase)
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=10000
+    BATCHSIZE=256; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
+    SAMPLES_PER_TASK=40000
+    ONLINE_ITER=0.25
+    EVAL_POINT="80000 160000 240000 320000 400000"
+
+elif [ "$DATASET" == "CUB_200" ]; then
+    MEM_SIZE=1000 # (changed 0901 - after 10x increase)
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=300
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
+    SAMPLES_PER_TASK=40000
+    ONLINE_ITER=2
+    EVAL_POINT="1000 2000 3000 4000 5994"
+
+elif [ "$DATASET" == "birds31" ]; then
+    MEM_SIZE=300 # (changed 0901 - after 10x increase)
+    N_SMP_CLS="9" K="3" MIR_CANDS=50
+    CANDIDATE_SIZE=50 VAL_SIZE=5
+    VAL_PERIOD=500 EVAL_PERIOD=100
+    BATCHSIZE=16; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=1
+    # Change vit learning rate (0611)
+    if [ "$MODEL_NAME" == "vit" ]; then
+        LR=1e-4
+        echo "Set vit learning rate 1e-4!!!"
+    fi
+    BASEINIT_SAMPLES=30523 FEAT_DIM=14 FEAT_MEM_SIZE=168000
+    SAMPLES_PER_TASK=40000
+    ONLINE_ITER=2
+    EVAL_POINT="236 475 714 953 1240"
+
+
+else
+    echo "Undefined setting"
+    exit 1
+fi
+
+LOG_DIR="logs"
+mkdir -p $LOG_DIR
+
+for RND_SEED in $SEEDS
+do
+    for index in "${!TYPES[@]}"
+    do
+    LOG_FILE="${LOG_DIR}/iclr_${MODEL_NAME}_${DATASET}_${MODE}_${TYPES[$index]}_sd${RND_SEED}.log"
+    echo "SEED: $RND_SEED"
+        CUDA_VISIBLE_DEVICES=${GPUS[$index]} nohup python eval_pretrained.py --mode $MODE $DATA_DIR \
+        --dataset $DATASET --unfreeze_rate $UNFREEZE_RATE $USE_KORNIA --k_coeff $K_COEFF --temperature $TEMPERATURE \
+        --sigma $SIGMA --repeat $REPEAT --init_cls $INIT_CLS --samples_per_task $SAMPLES_PER_TASK \
+        --rnd_seed $RND_SEED --val_memory_size $VAL_SIZE --type_name "${TYPES[$index]}" \
+        --model_name $MODEL_NAME --opt_name $OPT_NAME --sched_name $SCHED_NAME \
+        --lr $LR --batchsize $BATCHSIZE --mir_cands $MIR_CANDS --eval_point "${EVAL_POINT}" \
+        --memory_size $MEM_SIZE $TRANSFORM_ON_GPU --online_iter $ONLINE_ITER \
+        --note $NOTE --eval_period $EVAL_PERIOD --imp_update_period $IMP_UPDATE_PERIOD $USE_AMP --n_worker $N_WORKER --future_steps $FUTURE_STEPS --eval_n_worker $EVAL_N_WORKER --eval_batch_size $EVAL_BATCH_SIZE \
+        --baseinit_samples $BASEINIT_SAMPLES --spatial_feat_dim $FEAT_DIM --feat_memsize $FEAT_MEM_SIZE > $LOG_FILE 2>&1 &
+    done
+done
