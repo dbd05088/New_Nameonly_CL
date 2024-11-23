ffd2404984130bf6f3a2af84d68a39b612bc1615
carpedkm <flytodk@gmail.com>
2024-11-24 01:59:47 +0900

    add Imagenet400

--- dataset/ImageNet_400_gdrive.sh
+++ dataset/ImageNet_400_gdrive.sh
@@ -8,33 +8,33 @@
 # rm ImageNet_c.tar
 
 
-# # Required
-mkdir -p ImageNet_400
-mkdir -p ImageNet
+# # # Required
+# mkdir -p ImageNet_400
+# mkdir -p ImageNet
 
-./gdrive files download 11XEooWsl5SSL4XgiXvDnDNOsQiz_Ebjb # ImageNet_r_50
-./gdrive files download 1NLRNn8HeC41JfidifkwUwL2OKKAH4ovB # ImageNet_test
-./gdrive files download 13VQ678_Cs2qnWLULQR4Vp5cK2XV4j1n5 # ImageNet_c_all
-./gdrive files download 1P23z_1FNJhyYS82eWVwnREIRkkkPuRIB # ImageNet_d_new
+# ./gdrive files download 11XEooWsl5SSL4XgiXvDnDNOsQiz_Ebjb # ImageNet_r_50
+# ./gdrive files download 1NLRNn8HeC41JfidifkwUwL2OKKAH4ovB # ImageNet_test
+# ./gdrive files download 13VQ678_Cs2qnWLULQR4Vp5cK2XV4j1n5 # ImageNet_c_all
+# ./gdrive files download 1P23z_1FNJhyYS82eWVwnREIRkkkPuRIB # ImageNet_d_new
 
-tar -xf ImageNet_r_50.tar -C ImageNet
-tar -xf ImageNet_test.tar -C ImageNet
-tar -xf ImageNet_c_all.tar -C ImageNet
-tar -xf ImageNet_d_new.tar -C ImageNet
-rm ImageNet_r_50.tar
-rm ImageNet_test.tar
-rm ImageNet_c_all.tar
-rm ImageNet_d_new.tar
+# tar -xf ImageNet_r_50.tar -C ImageNet
+# tar -xf ImageNet_test.tar -C ImageNet
+# tar -xf ImageNet_c_all.tar -C ImageNet
+# tar -xf ImageNet_d_new.tar -C ImageNet
+# rm ImageNet_r_50.tar
+# rm ImageNet_test.tar
+# rm ImageNet_c_all.tar
+# rm ImageNet_d_new.tar
 
-# # ImageNet train_ma 400
-./gdrive files download 1ZC1cfRtsXQNVKpI2hOT3_8NsGu5tbmbQ # ImageNet_train_400
-tar -xf ImageNet_train_400.tar -C ImageNet_400
-rm ImageNet_train_400.tar
+# # # ImageNet train_ma 400
+# ./gdrive files download 1pxRkAkMczWyOPVOh8DrIqPE-DAUmFmGz # ImageNet_400_train_ma
+# tar -xf ImageNet_400_train_ma.tar -C ImageNet_400
+# rm ImageNet_400_train_ma.tar
 
-# ImageNet ours 400
-./gdrive files download 1HhE3NZ9A_xXGAlGTO_E9gGQ58v2BTHr8 # ImageNet_50_2_full_sdxl_floyd_cogview2_sd3_auraflow
-tar -xf ImageNet_50_2_full_sdxl_floyd_cogview2_sd3_auraflow.tar -C ImageNet_400
-rm ImageNet_50_2_full_sdxl_floyd_cogview2_sd3_auraflow.tar
+# # ImageNet ours 400
+# ./gdrive files download 1t-jjjVjZZsou9wErbFJD72Kh7tVyMMdp # ImageNet_400_50_2_full_sdxl_floyd_cogview2_sd3_auraflow
+# tar -xf ImageNet_400_50_2_full_sdxl_floyd_cogview2_sd3_auraflow.tar -C ImageNet_400
+# rm ImageNet_400_50_2_full_sdxl_floyd_cogview2_sd3_auraflow.tar
 
 # ImageNet ours 400
 ./gdrive files download 1ZDMObIEmhcS7DPsQLBpsWmzelrEGkqV0 # ImageNet_400_glide_syn_400
